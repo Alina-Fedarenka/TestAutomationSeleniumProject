f8bd313058f6cb79179363d3753ad0155f8fee30
Alina Fedarenka <sharapovalina@gmail.com>
2023-09-28 16:27:55 -0400

    Initial commit

---
+++ tests/test_login_page_negative.py
@@ -0,0 +1,18 @@
+import pytest
+
+from page_objects.login_page import LoginPage
+
+
+class TestNegativeScenarios:
+
+    @pytest.mark.login
+    @pytest.mark.negative
+    # Type username incorrectUser into Username field
+    @pytest.mark.parametrize("username, password, expected_error_message",
+                             [("incorrectUser", "Password123", "Your username is invalid!"),
+                              ("student", "incorrectPassword", "Your password is invalid!")])
+    def test_negative_login(self, driver, username, password, expected_error_message):
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.execute_login(username, password)
+        assert login_page.get_error_message() == expected_error_message, "Error message is not expected"
